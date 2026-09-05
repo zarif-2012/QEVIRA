c01bbc7a9514cdae77dfc0835b58b3632164094b
zarif-2012 <nadeemzarifnizarproduction@gmail.com>
2026-09-05 12:29:56 +0530

    Update Supabase publishable key

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const SUPABASE_URL =
 // ======================================================
 
 const SUPABASE_PUBLISHABLE_KEY =
-  "PASTE_YOUR_EXISTING_PUBLISHABLE_KEY_HERE";
+  "sb_publishable_bD3ajWNbZPoUw4uUwYhK3w_P-iZIAhw";
 
 
 // ======================================================
